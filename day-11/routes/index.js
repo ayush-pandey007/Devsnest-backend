6cad08149fa5f5124c9f9c541bad5fc151ab252c
ayush-pandey007 <ayushpandey1151@gmail.com>
2021-09-29 02:34:55 +0530

    completed 🧄

---
+++ day-11/routes/index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+var registerInitailCheck = require("../middlewares/registerChecks");
+var register = require("../controllers/register");
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.post("/register", registerInitailCheck, register);
+module.exports = router;
